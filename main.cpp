22e8b6f729970d42e9c9d4dae8d577b32d04fb9f
Bernardo Gabriel Vitor <bernardo.gabriel@arcotech.io>
2023-12-03 00:34:09 -0300

    doubled value

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Diga um número: "; // ask user for a number
+    std::cout << "Enter an integer: ";
 
-    int x{};       // define variable x to hold user input (and value-initialize it)
-    std::cin >> x; // get number from keyboard and store it in variable x
+    int num{ };
+    std::cin >> num;
 
-    std::cout << "Você colocou o número: " << x << '\n';
+    std::cout << "Double that number is: " <<  num * 2 << '\n';
     return 0;
 }
