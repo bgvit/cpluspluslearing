a381cd18914bce76c63f681094f0be9aadbe5f59
Bernardo Gabriel Vitor <bernardo.gabriel@arcotech.io>
2023-12-02 23:01:16 -0300

    using cli to input and output values

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Diga um número: "; // ask user for a number
+
+    int x{};       // define variable x to hold user input (and value-initialize it)
+    std::cin >> x; // get number from keyboard and store it in variable x
+
+    std::cout << "Você colocou o número: " << x << '\n';
     return 0;
 }
